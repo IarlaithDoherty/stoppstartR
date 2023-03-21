7a6ad7ec903a6548244f0e9c1ca1494a8028f05f
UltanPDoherty <72346121+UltanPDoherty@users.noreply.github.com>
2023-03-21 16:04:34 +0000

    Created check_any_match.R

---
+++ R/check_any_match.R
@@ -0,0 +1,22 @@
+#' Check which rows of `df` contain any codes in `code_set` in the columns
+#' starting with `column_prefix`.
+#'
+#' @param df Dataframe of patient information.
+#' @param column_prefix Prefix of columns to be checked.
+#' @param code_set A character string of codes to check matches for, separated by "|".
+#'
+#' @return A logical vector indicating which rows / patients had a match in any column.
+#' @export
+#'
+#' @examples check_any_match(mock_patients, column_prefix = "Drug_", code_set = "C10AA")
+check_any_match <- function(df, column_prefix, code_set) {
+  columns <- grep(colnames(df), pattern = column_prefix)
+  column_checks <- lapply(X = df[, columns],
+                                  FUN = function(x){
+                                    grepl(x, pattern = code_set)
+                                  }
+    )
+  combined_check <- Reduce(x = column_checks, f = "|")
+
+  return(combined_check)
+}
