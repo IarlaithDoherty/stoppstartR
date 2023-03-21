9cfdccd9ab2eb365508f0c2faaab2ed7077ecc91
UltanPDoherty <72346121+UltanPDoherty@users.noreply.github.com>
2023-03-21 13:00:15 +0000

    start_a5: Added roxygen skeleton

--- R/start_a5.R
+++ R/start_a5.R
@@ -1,7 +1,17 @@
+#' Function to implement START-A5 rule.
+#'
+#' @param df Dataframe of patient information.
+#'
+#' @return List with two elements:
+#' `bool` is a logical vector indicating whether each patient has triggered START-A5,
+#' `text` is a character vector of the same length whose entries are either blank
+#' or contain the instruction message for START-A5.
+#' @export
+#'
 start_a5 <- function(df) {
   comorbs_cols <- paste0("Comorbidity_", 1:20)
   drugs_cols <- paste0("Drug_", 1:20)
-  
+
   extras_cond1 <- df$Age < 85
 
   comorbs_set1 <- c("I20", "I21", "I22", "I24", "I25")
