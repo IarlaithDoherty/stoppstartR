622bffcdd30ddf247dedda3d4c9d88df38fc925f
UltanPDoherty <72346121+UltanPDoherty@users.noreply.github.com>
2023-03-22 11:26:36 +0000

    check_any_match: code_set to code_string

--- R/check_any_match.R
+++ R/check_any_match.R
@@ -1,21 +1,24 @@
-#' Check which rows of `df` contain any codes in `code_set` in the columns
+#' @title Check which patients contain code strings in specified columns.
+#'
+#' @description
+#' Check which rows of `df` contain any codes in `code_string` in the columns
 #' starting with `column_string`.
 #'
 #' @param df Dataframe of patient information.
 #' @param column_string
 #'        Only columns with names containing this string will be checked.
-#' @param code_set
+#' @param code_string
 #'        A character string of codes to check matches for, separated by "|".
 #'
 #' @return A logical vector indicating which rows / patients from `df` had an
-#'         entry containing one of the codes in `code_set` in any of the columns
-#'         with names containing `column_string`.
+#'         entry containing one of the codes in `code_string` in any of the
+#'         columns with names containing `column_string`.
 #' @export
 #'
 #' @examples check_any_match(mock_patients,
 #'                           column_string = "Drug_",
-#'                           code_set = "C10AA")
-check_any_match <- function(df, column_string, code_set) {
+#'                           code_string = "C10AA")
+check_any_match <- function(df, column_string, code_string) {
 
   # 'columns' is an integer vector.
   # These are the numbers of columns with names containing 'column_string'.
@@ -23,10 +26,10 @@ check_any_match <- function(df, column_string, code_set) {
 
   # 'column_checks' is a list of logical vectors, one per column.
   # Each vector has one TRUE / FALSE value per patient.
-  # TRUE if that patient has any of the codes from 'code_set' in that column.
+  # TRUE if that patient has any of the codes from 'code_string' in that column.
   column_checks <- lapply(X = df[, columns],
                                   FUN = function(x) {
-                                    grepl(x, pattern = code_set)
+                                    grepl(x, pattern = code_string)
                                   }
                           )
 
