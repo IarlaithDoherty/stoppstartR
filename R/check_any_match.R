ab69d1dc888bb50f49765e1abd36ad8f9b3d6e5f
UltanPDoherty <72346121+UltanPDoherty@users.noreply.github.com>
2023-03-22 12:27:27 +0000

    check_any_match: swap arg. code_string to codes
    Input codes to be checked as a character vector and paste(..., collapse = "|") inside check_any_match().

--- R/check_any_match.R
+++ R/check_any_match.R
@@ -1,35 +1,39 @@
 #' @title Check which patients contain code strings in specified columns.
 #'
 #' @description
-#' Check which rows of `df` contain any codes in `code_string` in the columns
+#' Check which rows of `df` contain any codes in `codes` in the columns
 #' starting with `column_string`.
 #'
 #' @param df Dataframe of patient information.
 #' @param column_string
 #'        Only columns with names containing this string will be checked.
-#' @param code_string
-#'        A character string of codes to check matches for, separated by "|".
+#' @param codes
+#'        A character vector of codes to search for.
 #'
 #' @return A logical vector indicating which rows / patients from `df` had an
-#'         entry containing one of the codes in `code_string` in any of the
+#'         entry containing one of the codes in `codes` in any of the
 #'         columns with names containing `column_string`.
 #' @export
 #'
 #' @examples check_any_match(mock_patients,
 #'                           column_string = "Drug_",
-#'                           code_string = "C10AA")
-check_any_match <- function(df, column_string, code_string) {
+#'                           codes = "C10AA")
+check_any_match <- function(df, column_string, codes) {
 
   # 'columns' is an integer vector.
   # These are the numbers of columns with names containing 'column_string'.
   columns <- grep(colnames(df), pattern = column_string)
 
+  # Collapse the character vector, 'codes', into a single character string.
+  # The codes are separated by "|", allowing grepl to check for any of them.
+  codes_string <- paste(codes, collapse = "|")
+
   # 'column_checks' is a list of logical vectors, one per column.
   # Each vector has one TRUE / FALSE value per patient.
-  # TRUE if that patient has any of the codes from 'code_string' in that column.
+  # TRUE if that patient has any of the codes from 'codes' in that column.
   column_checks <- lapply(X = df[, columns],
                                   FUN = function(x) {
-                                    grepl(x, pattern = code_string)
+                                    grepl(x, pattern = codes_string)
                                   }
                           )
 
