3f14015b4827083fe33fe6082f4db832cfce97d9
UltanPDoherty <72346121+UltanPDoherty@users.noreply.github.com>
2023-03-22 10:43:06 +0000

    check_any_match: formatting, comments

--- R/check_any_match.R
+++ R/check_any_match.R
@@ -2,21 +2,36 @@
 #' starting with `column_string`.
 #'
 #' @param df Dataframe of patient information.
-#' @param code_set A character string of codes to check matches for, separated by "|".
 #' @param column_string
 #'        Only columns with names containing this string will be checked.
+#' @param code_set
+#'        A character string of codes to check matches for, separated by "|".
 #'
-#' @return A logical vector indicating which rows / patients had a match in any column.
+#' @return A logical vector indicating which rows / patients from `df` had an
+#'         entry containing one of the codes in `code_set` in any of the columns
+#'         with names containing `column_string`.
 #' @export
 #'
-#' @examples check_any_match(mock_patients, column_prefix = "Drug_", code_set = "C10AA")
-check_any_match <- function(df, column_prefix, code_set) {
-  columns <- grep(colnames(df), pattern = column_prefix)
+#' @examples check_any_match(mock_patients,
+#'                           column_string = "Drug_",
+#'                           code_set = "C10AA")
+check_any_match <- function(df, column_string, code_set) {
+
+  # 'columns' is an integer vector.
+  # These are the numbers of columns with names containing 'column_string'.
+  columns <- grep(colnames(df), pattern = column_string)
+
+  # 'column_checks' is a list of logical vectors, one per column.
+  # Each vector has one TRUE / FALSE value per patient.
+  # TRUE if that patient has any of the codes from 'code_set' in that column.
   column_checks <- lapply(X = df[, columns],
-                                  FUN = function(x){
+                                  FUN = function(x) {
                                     grepl(x, pattern = code_set)
                                   }
-    )
+                          )
+
+  # 'combined_check' is a logical vector with a TRUE / FALSE value per patient.
+  # TRUE if that patient has any TRUE values in 'column_checks'.
   combined_check <- Reduce(x = column_checks, f = "|")
 
   return(combined_check)
