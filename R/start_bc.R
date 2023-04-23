3e395caae11d945920bbca8241b46d19c2aa59c4
IarlaithDoherty <127618196+IarlaithDoherty@users.noreply.github.com>
2023-04-23 13:15:31 +0100

    added start_c4

--- R/start_bc.R
+++ R/start_bc.R
@@ -297,7 +297,12 @@ start_c3 <- function(df, comorb_string = "Comorbidity_",
 #'
 #' START-C4 requires all of the following conditions to be satisfied:
 #' \itemize{
-#' \item
+#' \item Any of the following comorbidities:
+#'
+#' H40.1
+#' \item None of the following drugs:
+#'
+#' S01EE or S01ED
 #' }
 #'
 #' @param df Dataframe of patient information.
@@ -306,14 +311,68 @@ start_c3 <- function(df, comorb_string = "Comorbidity_",
 #' @param drug_string Character string contained in the name of each drug
 #'                    column which uniquely identifies them.
 #'
-#' @inherit start_c1 return
+#' @return `output`: character vector,
+#' \itemize{
+#' \item "Not Relevant" if the conditions are not satisfied.
+#' \item "Appropriate" if the conditions are satisfied but the correct drug has
+#' already been prescribed.
+#' \item The name of the criterion if the conditions are satisfied and the
+#' correct drug has not been prescribed.
+#' }
 #'
 #' @export
 start_c4 <- function(df, comorb_string = "Comorbidity_",
                      drug_string = "Drug_") {
+  if (!any(grepl(colnames(df), pattern = comorb_string))) {
+    stop(paste0("No column names include ", comorb_string,
+                ". Change comorb_string argument."))
+  } else if (!any(grepl(colnames(df), pattern = drug_string))) {
+    stop(paste0("No column names include ", drug_string,
+                ". Change drug_string argument."))
+  }
 
 
+  # prelim_checks is a list of logical vectors, each has one entry per patient.
+  prelim_checks <- list()
+  # prelim_codes is a list of character vectors, each containing codes to check.
+  prelim_codes <- list()
 
+  # prelim_codes$comorbs1 is a character vector of comorbidity codes to check.
+  prelim_codes$comorbs1 <- c("H40.1")
+  # prelim_checks$comorbs1 is TRUE if the patient has any listed comorbidities.
+  prelim_checks$comorbs1 <- check_matches(df,
+                                          column_string = comorb_string,
+                                          codes = prelim_codes$comorbs1,
+                                          match = "any")
+
+  # all_prelims is a logical vector with one entry per patient.
+  # TRUE if the patient is TRUE for each element of 'prelim_checks'.
+  all_prelims <- Reduce(x = prelim_checks, f = "&")
+
+
+  # action_checks is a list of logical vectors, each has one entry per patient.
+  action_checks <- list()
+  # action_codes is a list of character vectors, each containing codes to check.
+  action_codes <- list()
+
+  # prelim_codes$drugs1 is a character vector of drug codes to check.
+  action_codes$drugs1 <- c("S01EE", "S01ED")
+  # prelim_checks$drugs1 is TRUE if the patient is on any listed drugs.
+  action_checks$drugs1 <- check_matches(df,
+                                        column_string = drug_string,
+                                        codes = action_codes$drugs1,
+                                        match = "any")
+
+  # all_actions is a logical vector with one entry per patient.
+  # TRUE if the patient is TRUE for each element of 'action_checks'.
+  all_actions <- Reduce(x = action_checks, f = "&")
+
+
+  output <- ifelse(all_prelims,
+                   ifelse(all_actions, "Appropriate", "START-C4"),
+                   "Not Relevant")
+
+  return(output)
 }
 
 
