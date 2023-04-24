21358ff9482d70b61c9cc0d689d03d9c8fe434c8
UltanPDoherty <72346121+UltanPDoherty@users.noreply.github.com>
2023-04-24 18:48:46 +0100

    test-start_a: tidying

--- tests/testthat/test-start_a.R
+++ tests/testthat/test-start_a.R
@@ -1,11 +1,13 @@
 test_that("start_a functions are true for corresponding rows of
           trigger_start_a", {
-  expect_equal(diag(rbind(
-    start_a1(trigger_start_a),
-    start_a3(trigger_start_a),
-    start_a4(trigger_start_a),
-    start_a5(trigger_start_a),
-    start_a6(trigger_start_a),
-    start_a7(trigger_start_a),
-    start_a8(trigger_start_a))), trigger_start_a[, 1])
+  expect_equal(
+    diag(rbind(
+      start_a1(trigger_start_a),
+      start_a3(trigger_start_a),
+      start_a4(trigger_start_a),
+      start_a5(trigger_start_a),
+      start_a6(trigger_start_a),
+      start_a7(trigger_start_a),
+      start_a8(trigger_start_a))),
+    trigger_start_a[, 1])
 })
