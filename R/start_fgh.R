3e9b53bca0c102b6e747fe98eb224eb0fb3b4be1
IarlaithDoherty <127618196+IarlaithDoherty@users.noreply.github.com>
2023-04-26 19:08:53 +0100

    added start_g2

--- R/start_fgh.R
+++ R/start_fgh.R
@@ -90,3 +90,103 @@ start_g1 <- function(df, gender_column = "Gender", comorb_string = "Comorbidity_
 
   return(output)
 }
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+#' @title Function to implement START-G2 criterion.
+#'
+#' @description
+#' Determine which patients triggered the conditions defining START-G2.
+#'
+#' START-G2 requires all of the following conditions to be satisfied:
+#' \itemize{
+#' \item Gender equals Male.
+#' \item Any of the following comorbidities:
+#'
+#' N40 or R33.
+#' \item None of the following drugs:
+#'
+#' G04CB.
+#' }
+#'
+#' @param df Dataframe of patient information.
+#' @param comorb_string Character string contained in the name of each
+#'                      comorbidity column which uniquely identifies them.
+#' @param drug_string Character string contained in the name of each drug
+#'                    column which uniquely identifies them.
+#' @param gender_column The name of the patient gender column as a character
+#'                    string.
+#'
+#' @return `output`: character vector,
+#' \itemize{
+#' \item "Not Relevant" if the conditions are not satisfied.
+#' \item "Appropriate" if the conditions are satisfied but the correct drug has
+#' already been prescribed.
+#' \item The name of the criterion if the conditions are satisfied and the
+#' correct drug has not been prescribed.
+#' }
+#'
+#' @export
+start_g2 <- function(df, gender_column = "Gender", comorb_string = "Comorbidity_",
+                     drug_string = "Drug_") {
+
+  if (!any(grepl(colnames(df), pattern = comorb_string))) {
+    stop(paste0("No column names include ", comorb_string,
+                ". Change comorb_string argument."))
+  } else if (!any(grepl(colnames(df), pattern = drug_string))) {
+    stop(paste0("No column names include ", drug_string,
+                ". Change drug_string argument."))
+  } else if (!(gender_column %in% colnames(df))) {
+    stop(paste0("No columns are named ", gender_column,
+                ". Change gender_column argument."))
+  }
+
+  # prelim_checks is a list of logical vectors, each has one entry per patient.
+  prelim_checks <- list()
+  # prelim_codes is a list of character vectors, each containing codes to check.
+  prelim_codes <- list()
+
+  # prelim_checks$extras1 is TRUE if the patient's gender is male.
+  prelim_checks$extras1 <- df[, gender_column, drop = TRUE] == "M"
+
+  # prelim_codes$comorbs1 is a character vector of comorbidity codes to check.
+  prelim_codes$comorbs1 <- c("N40", "R33")
+  # prelim_checks$comorbs1 is TRUE if the patient has any listed comorbidities.
+  prelim_checks$comorbs1 <- check_matches(df,
+                                          column_string = comorb_string,
+                                          codes = prelim_codes$comorbs1,
+                                          match = "any")
+
+  # all_prelims is a logical vector with one entry per patient.
+  # TRUE if the patient is TRUE for each element of 'prelim_checks'.
+  all_prelims <- Reduce(x = prelim_checks, f = "&")
+
+  # action_checks is a list of logical vectors, each has one entry per patient.
+  action_checks <- list()
+  # action_codes is a list of character vectors, each containing codes to check.
+  action_codes <- list()
+
+  # prelim_codes$drugs1 is a character vector of drug codes to check.
+  action_codes$drugs1 <- c("G04CB")
+  # prelim_checks$drugs1 is TRUE if the patient is on any listed drugs.
+  action_checks$drugs1 <- check_matches(df,
+                                        column_string = drug_string,
+                                        codes = action_codes$drugs1,
+                                        match = "any")
+
+  # all_actions is a logical vector with one entry per patient.
+  # TRUE if the patient is TRUE for each element of 'action_checks'.
+  all_actions <- Reduce(x = action_checks, f = "&")
+
+
+  output <- ifelse(all_prelims,
+                   ifelse(all_actions, "Appropriate", "START-g2"),
+                   "Not Relevant")
+
+  return(output)
+}
+
+
+
