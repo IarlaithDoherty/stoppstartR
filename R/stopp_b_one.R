0124d049cba180f6f987016a51bc0bfde3326250
IarlaithDoherty <127618196+IarlaithDoherty@users.noreply.github.com>
2023-05-01 13:23:56 +0100

    added stopp_b4

--- R/stopp_b_one.R
+++ R/stopp_b_one.R
@@ -276,6 +276,107 @@ stopp_b3 <- function(df, drug_string = "Drug_") {
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#'
+#' @title Function to implement STOPP-B4 criterion.
+#'
+#' @description
+#' Determine which patients triggered the conditions defining STOPP-B4.
+#'
+#' STOPP-B4 requires the following conditions to be satisfied:
+#' \itemize{
+#' \item Heart Rate less than 50 beats per minute.
+#' OR
+#' \item Any of the following comorbidities:
+#'
+#'I49.5, R00.1, I44.1, I44.2, I44.3, I45.5, I45.9, Q24.6
+#' \item Any of the following drugs:
+#'
+#' C07.
+#' }
+#'
+#' @param df Dataframe of patient information.
+#' @param comorb_string Character string contained in the name of each
+#'                      comorbidity column which uniquely identifies them.
+#' @param drug_string Character string contained in the name of each drug
+#'                    column which uniquely identifies them.
+#' @param HR_column The name of the patient heart rate column as a character
+#' string.
+#'
+#' @return `output`: character vector,
+#' \itemize{
+#' \item "Not Relevant" if the conditions are not satisfied.
+#' \item "Appropriate" if the conditions are satisfied but the correct drug has
+#' already been prescribed.
+#' \item The name of the criterion if the conditions are satisfied and the
+#' correct drug has not been prescribed.
+#' }
+#'
+#' @export
+stopp_b4 <- function(df, HR_column = "Lab Values: Heart Rate",
+                     comorb_string = "Comorbidity_",
+                     drug_string = "Drug_") {
+
+  if (!any(grepl(colnames(df), pattern = comorb_string))) {
+    stop(paste0("No column names include ", comorb_string,
+                ". Change comorb_string argument."))
+  } else if (!any(grepl(colnames(df), pattern = drug_string))) {
+    stop(paste0("No column names include ", drug_string,
+                ". Change drug_string argument."))
+  } else if (!(HR_column %in% colnames(df))) {
+    stop(paste0("No columns are named ", HR_column,
+                ". Change HR_column argument."))
+  }
+
+  # prelim_checks is a list of logical vectors, each has one entry per patient.
+  prelim_checks <- list()
+  # prelim_codes is a list of character vectors, each containing codes to check.
+  prelim_codes <- list()
+
+  # prelim_checks$extras1 is TRUE if the patient's HR is less than 50 beats per
+  # minute.
+  prelim_checks$extras1 <- df[, HR_column, drop = TRUE] < 50
+
+  # prelim_codes$comorbs1 is a character vector of comorbidity codes to check.
+  prelim_codes$comorbs1 <- c("I49.5", "R00.1", "I44.1", "I44.2", "I44.3",
+  "I45.5", "I45.9", "Q24.6")
+  # prelim_checks$comorbs1 is TRUE if the patient has any listed comorbidities.
+  prelim_checks$comorbs1 <- check_matches(df,
+                                          column_string = comorb_string,
+                                          codes = prelim_codes$comorbs1,
+                                          match = "any")
+
+  # all_prelims is a logical vector with one entry per patient.
+  # TRUE if the patient is TRUE for either element of 'prelim_checks'.
+  all_prelims <- Reduce(x = prelim_checks, f = "|")
+
+  # action_checks is a list of logical vectors, each has one entry per patient.
+  action_checks <- list()
+  # action_codes is a list of character vectors, each containing codes to check.
+  action_codes <- list()
+
+  # prelim_codes$drugs1 is a character vector of drug codes to check.
+  action_codes$drugs1 <- c("C07")
+  # prelim_checks$drugs1 is TRUE if the patient is on none of listed drugs.
+  action_checks$drugs1 <- check_matches(df,
+                                        column_string = drug_string,
+                                        codes = action_codes$drugs1,
+                                        match = "none")
+
+  # all_actions is a logical vector with one entry per patient.
+  # TRUE if the patient is TRUE for each element of 'action_checks'.
+  all_actions <- Reduce(x = action_checks, f = "&")
+
+
+  output <- ifelse(all_prelims,
+                   ifelse(all_actions, "Appropriate", "STOPP-B4"),
+                   "Not Relevant")
+
+  return(output)
+}
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 
 
 #' @title Function to implement STOPP-B5 criterion.
