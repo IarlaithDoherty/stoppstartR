1f43c48a6be14a4288329a55511e61ada5fcc659
IarlaithDoherty <127618196+IarlaithDoherty@users.noreply.github.com>
2023-07-30 15:34:22 +0100

    wrote stopp_j1 and stopp_j2

--- R/stopp_ij.R
+++ R/stopp_ij.R
@@ -152,3 +152,98 @@ stopp_j1 <- function(df,
   return(output)
 }
 
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+#' @title Function to implement STOPP-J2 criterion.
+#'
+#' @description
+#' Determine which patients triggered the conditions defining STOPP-J2.
+#'
+#' STOPP-J2 requires all of the following conditions to be satisfied:
+#' \itemize{
+#' \item Any of the following comorbidities:
+#'
+#' I50, I11.0, I13.0, I13.2
+#' \item Any of the following drugs:
+#'
+#' A10BG, A10BD04, A10BD03, A10BD06, A10BD05, A10BD09, A10BD12
+#' }
+#'
+#' @param df Dataframe of patient information.
+#' @param comorb_string Character string contained in the name of each
+#'                      comorbidity column which uniquely identifies them.
+#' @param drug_string Character string contained in the name of each drug
+#'                    column which uniquely identifies them.
+#'
+#' @return `output`: character vector,
+#' \itemize{
+#' \item "Not Relevant" if the conditions are not satisfied.
+#' \item "Appropriate" if the conditions are satisfied but the correct drug has
+#' already been prescribed.
+#' \item The name of the criterion if the conditions are satisfied and the
+#' correct drug has not been prescribed.
+#' }
+#'
+#' @export
+stopp_j2 <- function(df, comorb_string = "Comorbidity_",
+                     drug_string = "Drug_") {
+  if (!any(grepl(colnames(df), pattern = comorb_string))) {
+    stop(paste0("No column names include ", comorb_string,
+                ". Change comorb_string argument."))
+  } else if (!any(grepl(colnames(df), pattern = drug_string))) {
+    stop(paste0("No column names include ", drug_string,
+                ". Change drug_string argument."))
+  }
+
+
+  # prelim_checks is a list of logical vectors, each has one entry per patient.
+  prelim_checks <- list()
+  # prelim_codes is a list of character vectors, each containing codes to check.
+  prelim_codes <- list()
+
+  # prelim_codes$comorbs1 is a character vector of comorbidity codes to check.
+  prelim_codes$comorbs1 <- c("I50", "I11.0", "I13.0", "I13.2"
+  )
+  # prelim_checks$comorbs1 is TRUE if the patient has any listed comorbidities.
+  prelim_checks$comorbs1 <- check_matches(df,
+                                          column_string = comorb_string,
+                                          codes = prelim_codes$comorbs1,
+                                          match = "any")
+
+  # all_prelims is a logical vector with one entry per patient.
+  # TRUE if the patient is TRUE for each element of 'prelim_checks'.
+  all_prelims <- Reduce(x = prelim_checks, f = "&")
+
+
+  # action_checks is a list of logical vectors, each has one entry per patient.
+  action_checks <- list()
+  # action_codes is a list of character vectors, each containing codes to check.
+  action_codes <- list()
+
+  # action_codes$drugs1 is a character vector of drug codes to check.
+  action_codes$drugs1 <- c("A10BG", "A10BD04", "A10BD03", "A10BD06", "A10BD05",
+                           "A10BD09", "A10BD12"
+  )
+  # action_checks$drugs1 is TRUE if the patient is on none listed drugs.
+  action_checks$drugs1 <- check_matches(df,
+                                        column_string = drug_string,
+                                        codes = action_codes$drugs1,
+                                        match = "none")
+
+  # all_actions is a logical vector with one entry per patient.
+  # TRUE if the patient is TRUE for each element of 'action_checks'.
+  all_actions <- Reduce(x = action_checks, f = "&")
+
+
+  output <- ifelse(all_prelims,
+                   ifelse(all_actions, "Appropriate", "STOPP-J2"),
+                   "Not Relevant")
+
+  return(output)
+}
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
