8537e887a36dbb35fa5e67f45ee09d3e92d7b640
IarlaithDoherty <127618196+IarlaithDoherty@users.noreply.github.com>
2023-08-01 19:55:18 +0100

    wrote stopp_d4

--- R/stopp_d_one.R
+++ R/stopp_d_one.R
@@ -172,7 +172,90 @@ stopp_d3 <- function(df, comorb_string = "Comorbidity_",
 
   return(output)
 }
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#' @title Function to implement STOPP-D4 criterion.
+#'
+#' @description
+#' Determine which patients triggered the conditions defining STOPP-D4.
+#'
+#' STOPP-D4 requires all of the following conditions to be satisfied:
+#' \itemize{
+#' \item Serum sodium less than:
+#'
+#' 135
+#' \item Any of the following drugs:
+#'
+#' N06AB, N06CA03
+#' }
+#'
+#' @param df Dataframe of patient information.
+#' @param Na_column The name of the patient Na column as a character string.
 
+#' @param drug_string Character string contained in the name of each drug
+#'                    column which uniquely identifies them.
+#'
+#' @return `output`: character vector,
+#' \itemize{
+#' \item "Not Relevant" if the conditions are not satisfied.
+#' \item "Appropriate" if the conditions are satisfied but the correct drug has
+#' already been prescribed.
+#' \item The name of the criterion if the conditions are satisfied and the
+#' correct drug has not been prescribed.
+#' }
+#'
+#' @export
+stopp_d4 <- function(df, Na_column = "Lab Values: Na",
+                     drug_string = "Drug_") {
+  if (!any(grepl(colnames(df), pattern = "Lab Values: Na"
+))) {
+    stop(paste0("No column names include ", Na_column,
+                ". Change Na_column argument."))
+  } else if (!any(grepl(colnames(df), pattern = drug_string))) {
+    stop(paste0("No column names include ", drug_string,
+                ". Change drug_string argument."))
+  }
+
+
+  # prelim_checks is a list of logical vectors, each has one entry per patient.
+  prelim_checks <- list()
+  # prelim_codes is a list of character vectors, each containing codes to check.
+  prelim_codes <- list()
+
+  # prelim_checks$extras1 is TRUE if the patient's Na is less than 135 mmol/L.
+  prelim_checks$extras1 <- df[, Na_column, drop = TRUE] < 135
+
+
+  # all_prelims is a logical vector with one entry per patient.
+  # TRUE if the patient is TRUE for each element of 'prelim_checks'.
+  all_prelims <- Reduce(x = prelim_checks, f = "&")
+
+
+  # action_checks is a list of logical vectors, each has one entry per patient.
+  action_checks <- list()
+  # action_codes is a list of character vectors, each containing codes to check.
+  action_codes <- list()
+
+  # prelim_codes$drugs1 is a character vector of drug codes to check.
+  action_codes$drugs1 <- c("N06AB", "N06CA03")
+  # prelim_checks$drugs1 is TRUE if the patient is on none listed drugs.
+  action_checks$drugs1 <- check_matches(df,
+                                        column_string = drug_string,
+                                        codes = action_codes$drugs1,
+                                        match = "none")
+
+  # all_actions is a logical vector with one entry per patient.
+  # TRUE if the patient is TRUE for each element of 'action_checks'.
+  all_actions <- Reduce(x = action_checks, f = "&")
+
+
+  output <- ifelse(all_prelims,
+                   ifelse(all_actions, "Appropriate", "STOPP-D4"),
+                   "Not Relevant")
+
+  return(output)
+}
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
