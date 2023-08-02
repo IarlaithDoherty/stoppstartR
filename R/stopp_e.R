6a29d6d1088edaa935c0a9b9ee0ce3e2cc0c4cca
IarlaithDoherty <127618196+IarlaithDoherty@users.noreply.github.com>
2023-08-02 18:05:03 +0100

    wrote stopp_e3

--- R/stopp_e.R
+++ R/stopp_e.R
@@ -161,8 +161,8 @@ stopp_e2 <- function(df, GFR_column = "Lab Values: eGFR",
   # action_codes is a list of character vectors, each containing codes to check.
   action_codes <- list()
 
-  # action_checks$extras1 is TRUE if the patient's GFR is less than 30ml/min.
-  action_checks$extras1 <- df[, GFR_column, drop = TRUE] < 30
+  # action_checks$extras1 is TRUE if the patient's GFR is greater than 30ml/min.
+  action_checks$extras1 <- df[, GFR_column, drop = TRUE] > 30
 
   # all_actions is a logical vector with one entry per patient.
   # TRUE if the patient is TRUE for each element of 'action_checks'.
@@ -171,6 +171,88 @@ stopp_e2 <- function(df, GFR_column = "Lab Values: eGFR",
 
   output <- ifelse(all_prelims,
                    ifelse(all_actions, "Appropriate", "STOPP-E2"),
+                   "Not Relevant")
+
+  return(output)
+}
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#' @title Function to implement STOPP-E3 criterion.
+#'
+#' @description
+#' Determine which patients triggered the conditions defining STOPP-E3.
+#'
+#' STOPP-E3 requires all of the following conditions to be satisfied:
+#' \itemize{
+#' \item Any of the following drugs:
+#'
+#' B01AF
+#' \item GFR less than:
+#'
+#' 15
+#' }
+#'
+#' @param df Dataframe of patient information.
+#' @param GFR_column The name of the patient Na column as a character string.
+#' @param drug_string Character string contained in the name of each drug
+#'                    column which uniquely identifies them.
+#'
+#' @return `output`: character vector,
+#' \itemize{
+#' \item "Not Relevant" if the conditions are not satisfied.
+#' \item "Appropriate" if the conditions are satisfied but the correct drug has
+#' already been prescribed.
+#' \item The name of the criterion if the conditions are satisfied and the
+#' correct drug has not been prescribed.
+#' }
+#'
+#' @export
+stopp_e3 <- function(df, GFR_column = "Lab Values: eGFR",
+                     drug_string = "Drug_") {
+
+  if (!any(grepl(colnames(df), pattern = GFR_column))) {
+    stop(paste0("No column names include ", GFR_column,
+                ". Change GFR_string argument."))
+  } else if (!any(grepl(colnames(df), pattern = drug_string))) {
+    stop(paste0("No column names include ", drug_string,
+                ". Change drug_string argument."))
+  }
+
+
+  # prelim_checks is a list of logical vectors, each has one entry per patient.
+  prelim_checks <- list()
+  # prelim_codes is a list of character vectors, each containing codes to check.
+  prelim_codes <- list()
+
+  # prelim_codes$drugs1 is a character vector of drug codes to check.
+  prelim_codes$drugs1 <- c("B01AF")
+  # prelim_checks$drugs1 is TRUE if the patient has any listed drugs.
+  prelim_checks$drugs1 <- check_matches(df,
+                                        column_string = drug_string,
+                                        codes = prelim_codes$drugs1,
+                                        match = "any")
+
+  # all_prelims is a logical vector with one entry per patient.
+  # TRUE if the patient is TRUE for each element of 'prelim_checks'.
+  all_prelims <- Reduce(x = prelim_checks, f = "&")
+
+
+  # action_checks is a list of logical vectors, each has one entry per patient.
+  action_checks <- list()
+  # action_codes is a list of character vectors, each containing codes to check.
+  action_codes <- list()
+
+  # action_checks$extras1 is TRUE if the patient's GFR is greater than 15ml/min.
+  action_checks$extras1 <- df[, GFR_column, drop = TRUE] > 15
+
+  # all_actions is a logical vector with one entry per patient.
+  # TRUE if the patient is TRUE for each element of 'action_checks'.
+  all_actions <- Reduce(x = action_checks, f = "&")
+
+
+  output <- ifelse(all_prelims,
+                   ifelse(all_actions, "Appropriate", "STOPP-E3"),
                    "Not Relevant")
 
   return(output)
