0e17784cb19f4a943c5eba2b1528c62ea7c67092
IarlaithDoherty <127618196+IarlaithDoherty@users.noreply.github.com>
2023-08-02 17:48:29 +0100

    wrote stopp_e1

---
+++ R/stopp_e.R
@@ -0,0 +1,98 @@
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#' @title Function to implement STOPP-E1 criterion.
+#'
+#' @description
+#' Determine which patients triggered the conditions defining STOPP-E1.
+#'
+#' STOPP-E1 requires all of the following conditions to be satisfied:
+#' \itemize{
+#' \item GFR less than:
+#'
+#' 30
+#' \item Any of the following drugs:
+#'
+#' C01AA05
+#' \item Digoxin Dose >125mcg:
+#'
+#' Yes
+#' }
+#'
+#' @param df Dataframe of patient information.
+#' @param GFR_column The name of the patient Na column as a character string.
+#' @param digoxin_column The name of the patient Digoxin column as a
+#' character string.
+#' @param drug_string Character string contained in the name of each drug
+#'                    column which uniquely identifies them.
+#'
+#' @return `output`: character vector,
+#' \itemize{
+#' \item "Not Relevant" if the conditions are not satisfied.
+#' \item "Appropriate" if the conditions are satisfied but the correct drug has
+#' already been prescribed.
+#' \item The name of the criterion if the conditions are satisfied and the
+#' correct drug has not been prescribed.
+#' }
+#'
+#' @export
+stopp_e1 <- function(df, GFR_column = "Lab Values: eGFR", digoxin_column =
+                    "Digoxin Dose >125mcg",
+                     drug_string = "Drug_") {
+
+  if (!any(grepl(colnames(df), pattern = GFR_column))) {
+    stop(paste0("No column names include ", GFR_column,
+                ". Change GFR_string argument."))
+  } else if (!any(grepl(colnames(df), pattern = drug_string))) {
+    stop(paste0("No column names include ", drug_string,
+                ". Change drug_string argument."))
+  } else if (!(digoxin_column %in% colnames(df))) {
+    stop(paste0("No columns are named ", digoxin_column,
+                ". Change digoxin_column argument."))
+  }
+
+
+  # prelim_checks is a list of logical vectors, each has one entry per patient.
+  prelim_checks <- list()
+  # prelim_codes is a list of character vectors, each containing codes to check.
+  prelim_codes <- list()
+
+  # prelim_checks$extras1 is TRUE if the patient's GFR is less than 30ml/min.
+  prelim_checks$extras1 <- df[, GFR_column, drop = TRUE] < 30
+
+  # prelim_codes$drugs1 is a character vector of drug codes to check.
+  prelim_codes$drugs1 <- c("C01AA05")
+  # prelim_checks$drugs1 is TRUE if the patient has any listed drugs.
+  prelim_checks$drugs1 <- check_matches(df,
+                                        column_string = drug_string,
+                                        codes = prelim_codes$drugs1,
+                                        match = "any")
+
+  # all_prelims is a logical vector with one entry per patient.
+  # TRUE if the patient is TRUE for each element of 'prelim_checks'.
+  all_prelims <- Reduce(x = prelim_checks, f = "&")
+
+
+  # action_checks is a list of logical vectors, each has one entry per patient.
+  action_checks <- list()
+  # action_codes is a list of character vectors, each containing codes to check.
+  action_codes <- list()
+
+  # action_checks$extras1 is TRUE if the patient's digoxin dose is greater than
+  # 125mcg.
+  action_checks$extras1 <- df[, digoxin_column, drop = TRUE] == "Yes"
+
+  # all_actions is a logical vector with one entry per patient.
+  # TRUE if the patient is TRUE for each element of 'action_checks'.
+  all_actions <- Reduce(x = action_checks, f = "&")
+
+
+  output <- ifelse(all_prelims,
+                   ifelse(all_actions, "Appropriate", "STOPP-E1"),
+                   "Not Relevant")
+
+  return(output)
+}
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
