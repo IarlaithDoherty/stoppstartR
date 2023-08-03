a2dfe668a9df8e81908717db1c908b15d6fcfc70
UltanPDoherty <72346121+UltanPDoherty@users.noreply.github.com>
2023-08-03 17:19:36 +0100

    Created test files for all start_bc functions

---
+++ tests/testthat/test-start_c1.R
@@ -0,0 +1,15 @@
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Basic Test -------------------------------------------------------------------
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+start_c1_df <- data.frame(
+  Comorbidity_1 = c("G20",  "G20", NA),
+  Drug_1        = c(   NA, "N04B", NA))
+
+test_that("start_c1 works", {
+  expect_equal(
+    start_c1(start_c1_df),
+    c("START-C1", "Appropriate", "Not Relevant"))
+})
+
+# No need for comprehensive test as the above basic test covers all scenarios.
