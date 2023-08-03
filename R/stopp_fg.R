74048dc9df786d6fa7bc17d657b3f70f874045e5
IarlaithDoherty <127618196+IarlaithDoherty@users.noreply.github.com>
2023-08-03 19:12:46 +0100

    wrote stopp_f2

--- R/stopp_fg.R
+++ R/stopp_fg.R
@@ -84,6 +84,91 @@ stopp_f1 <- function(df, comorb_string = "Comorbidity_",
 
   return(output)
 }
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+#' @title Function to implement STOPP-F2 criterion.
+#'
+#' @description
+#' Determine which patients triggered the conditions defining STOPP-F2.
+#'
+#' STOPP-F2 requires all of the following conditions to be satisfied:
+#' \itemize{
+#' \item Any of the following drugs:
+#'
+#' A02BC
+#'
+#' \item Full Dose PPI =
+#'
+#' Yes.
+#'}
+#'
+#' @param df Dataframe of patient information.
+#' @param drug_string Character string contained in the name of each drug
+#'                    column which uniquely identifies them.
+#' @param ppi_column The name of the patient PPI column as a
+#' character string.
+#'
+#' @return `output`: character vector,
+#' \itemize{
+#' \item "Not Relevant" if the conditions are not satisfied.
+#' \item "Appropriate" if the conditions are satisfied but the correct drug has
+#' already been prescribed.
+#' \item The name of the criterion if the conditions are satisfied and the
+#' correct drug has not been prescribed.
+#' }
+#'
+#' @export
+stopp_f2 <- function(df, ppi_column = "Full Dose PPI",
+                     drug_string = "Drug_") {
+  if (!any(grepl(colnames(df), pattern = ppi_column))) {
+    stop(paste0("No column names include ", ppi_column,
+                ". Change ppi_column argument."))
+  } else if (!any(grepl(colnames(df), pattern = drug_string))) {
+    stop(paste0("No column names include ", drug_string,
+                ". Change drug_string argument."))
+  }
+
+
+  # prelim_checks is a list of logical vectors, each has one entry per patient.
+  prelim_checks <- list()
+  # prelim_codes is a list of character vectors, each containing codes to check.
+  prelim_codes <- list()
+
+  # prelim_codes$drugs1 is a character vector of drug codes to check.
+  prelim_codes$drugs1 <- c("A02BC")
+  # prelim_checks$drugs1 is TRUE if the patient has any listed drugs.
+  prelim_checks$drugs1 <- check_matches(df,
+                                        column_string = drug_string,
+                                        codes = prelim_codes$drugs1,
+                                        match = "any")
+
+  # all_prelims is a logical vector with one entry per patient.
+  # TRUE if the patient is TRUE for each element of 'prelim_checks'.
+  all_prelims <- Reduce(x = prelim_checks, f = "&")
+
+
+  # action_checks is a list of logical vectors, each has one entry per patient.
+  action_checks <- list()
+  # action_codes is a list of character vectors, each containing codes to check.
+  action_codes <- list()
+
+  # action_checks$extras1 is True if the patient's aspirin dose column is
+  # not equal to "Yes"
+  action_checks$extras1 <- df[, ppi_column, drop = TRUE] != "Yes"
+
+  # all_actions is a logical vector with one entry per patient.
+  # TRUE if the patient is TRUE for each element of 'action_checks'.
+  all_actions <- Reduce(x = action_checks, f = "&")
+
+
+  output <- ifelse(all_prelims,
+                   ifelse(all_actions, "Appropriate", "STOPP_F2"),
+                   "Not Relevant")
+
+  return(output)
+}
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
