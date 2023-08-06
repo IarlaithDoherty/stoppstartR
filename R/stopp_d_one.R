9eb7823873d4871df6cec0a7a3086ed34c1141c0
IarlaithDoherty <127618196+IarlaithDoherty@users.noreply.github.com>
2023-08-06 18:36:58 +0100

    wrote stopp_d7

--- R/stopp_d_one.R
+++ R/stopp_d_one.R
@@ -407,10 +407,96 @@ stopp_d6 <- function(df, comorb_string = "Comorbidity_",
                    "Not Relevant")
   return(output)
 }
+
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+#' @title Function to implement STOPP-D7 criterion.
+#'
+#' @description
+#' Determine which patients triggered the conditions defining STOPP-D7.
+#'
+#' STOPP-D7 requires all of the following conditions to be satisfied:
+#' \itemize{
+#' \item Any of the following drugs:
+#'
+#' N05A, except N05AN
+
+#' \item Any of the following drugs:
+#'
+#' N04A
+#' }
+#'
+#' @param df Dataframe of patient information.
+#' @param drug_string Character string contained in the name of each drug
+#'                    column which uniquely identifies them.
+#'
+#' @return `output`: character vector,
+#' \itemize{
+#' \item "Not Relevant" if the conditions are not satisfied.
+#' \item "Appropriate" if the conditions are satisfied but the correct drug has
+#' already been prescribed.
+#' \item The name of the criterion if the conditions are satisfied and the
+#' correct drug has not been prescribed.
+#' }
+#'
+#' @export
+stopp_d7 <- function(df, drug_string = "Drug_") {
+  if (!any(grepl(colnames(df), pattern = drug_string))) {
+    stop(paste0("No column names include ", drug_string,
+                ". Change drug_string argument."))
+  }
+
+
+  # prelim_checks is a list of logical vectors, each has one entry per patient.
+  prelim_checks <- list()
+  # prelim_codes is a list of character vectors, each containing codes to check.
+  prelim_codes <- list()
+
+  # prelim_codes$drugs1 is a character vector of comorbidity codes to check.
+  prelim_codes$drugs1 <- c("N05A")
+  # prelim_checks$drugs1 is TRUE if the patient has any listed comorbidities.
+  prelim_checks$drugs1 <- check_matches(df,
+                                        column_string = drug_string,
+                                        codes = prelim_codes$drugs1,
+                                        match = "any",
+                                        exceptions = c("N05AN"))
+
+  # all_prelims is a logical vector with one entry per patient.
+  # TRUE if the patient is TRUE for each element of 'prelim_checks'.
+  all_prelims <- Reduce(x = prelim_checks, f = "&")
+
+
+  # action_checks is a list of logical vectors, each has one entry per patient.
+  action_checks <- list()
+  # action_codes is a list of character vectors, each containing codes to check.
+  action_codes <- list()
+
+  # action_codes$drugs1 is a character vector of drug codes to check.
+  action_codes$drugs1 <- c("N04A"
+  )
+  # action_checks$drugs1 is TRUE if the patient is on any listed drugs.
+  action_checks$drugs1 <- check_matches(df,
+                                        column_string = drug_string,
+                                        codes = action_codes$drugs1,
+                                        match = "none")
+
+  # all_actions is a logical vector with one entry per patient.
+  # TRUE if the patient is TRUE for each element of 'action_checks'.
+  all_actions <- Reduce(x = action_checks, f = "&")
+
+
+  output <- ifelse(all_prelims,
+                   ifelse(all_actions, "Appropriate", "STOPP-D7"),
+                   "Not Relevant")
+
+  return(output)
+}
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 #' @title Function to implement STOPP-D8 criterion.
 #'
