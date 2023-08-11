050c92c87ad759d3fd14c956b184009d1ec62179
IarlaithDoherty <127618196+IarlaithDoherty@users.noreply.github.com>
2023-08-11 12:22:50 +0100

    wrote stopp_m1

--- R/stopp_klm.R
+++ R/stopp_klm.R
@@ -390,7 +390,6 @@ stopp_l2 <- function(df, drug_string = "Drug_") {
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-
 #' @title Function to implement STOPP-L3 criterion.
 #'
 #' @description
@@ -471,6 +470,89 @@ stopp_l3 <- function(df, drug_string = "Drug_") {
   output <- ifelse(all_prelims,
                    ifelse(all_actions, "Appropriate", "STOPP-L3"),
                    "Not Relevant")
+
+  return(output)
+}
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+#' @title Function to implement STOPP-M1 criterion.
+#'
+#' @description
+#' Determine which patients triggered the conditions defining STOPP-M1.
+#'
+#' STOPP-M1 requires all of the following conditions to be satisfied:
+#' \itemize{
+
+#' \item More than two of the following drugs:
+#'
+#' R06AA, R06AB, R06AC, R06AD , R06AE01, R06AE03, R06AE04, R06AE05, R06AE06,
+#' R06AE51, R06AE53, R06AE55, R06AX01, R06AX02, R06AX03, R06AX04, R06AX05,
+#' R06AX08, R06AX09, R06AX15, R06AX16, R06AX17, R06AX23, R06AX53, R06AX58,
+#' N05BB01, N05BB51, N07CA02, N07CA52, G04BD01, G04BD02, G04BD03, G04BD04,
+#' G04BD05, G04BD06, G04BD07, G04BD08, G04BD09, G04BD10, G04BD11, N06AA, A03B,
+#' A03AA, A03AB, A04AD01, M03BX02, N04A, N05AA01, N05AH02, N05AB02, N05AA02,
+#' N05AC02, N05AA04, N05AF04
+#' }
+#'
+#' @param df Dataframe of patient information.
+#' @param drug_string Character string contained in the name of each drug
+#'                    column which uniquely identifies them.
+#'
+#' @return `output`: character vector,
+#' \itemize{
+#' \item "Not Relevant" if the conditions are not satisfied.
+#' \item "Appropriate" if the conditions are satisfied but the correct drug has
+#' already been prescribed.
+#' \item The name of the criterion if the conditions are satisfied and the
+#' correct drug has not been prescribed.
+#' }
+#'
+#' @export
+stopp_m1 <- function(df, drug_string = "Drug_") {
+  if (!any(grepl(colnames(df), pattern = drug_string))) {
+    stop(paste0("No column names include ", drug_string,
+                ". Change drug_string argument."))
+  }
+
+
+  # prelim_checks is a list of logical vectors, each has one entry per patient.
+  prelim_checks <- list()
+  # prelim_codes is a list of character vectors, each containing codes to check.
+  prelim_codes <- list()
+
+  # prelim_codes$drugs1 is a character vector of drug codes to check.
+  prelim_codes$drugs1 <- c("R06AA", "R06AB", "R06AC", "R06AD" , "R06AE01",
+                           "R06AE03", "R06AE04", "R06AE05", "R06AE06",
+                           "R06AE51", "R06AE53", "R06AE55", "R06AX01",
+                           "R06AX02", "R06AX03", "R06AX04", "R06AX05",
+                           "R06AX08", "R06AX09", "R06AX15", "R06AX16",
+                           "R06AX17", "R06AX23", "R06AX53", "R06AX58",
+                           "N05BB01", "N05BB51", "N07CA02", "N07CA52",
+                           "G04BD01", "G04BD02", "G04BD03", "G04BD04",
+                           "G04BD05", "G04BD06", "G04BD07", "G04BD08",
+                           "G04BD09", "G04BD10", "G04BD11", "N06AA", "A03B",
+                           "A03AA", "A03AB", "A04AD01", "M03BX02", "N04A",
+                           "N05AA01", "N05AH02", "N05AB02", "N05AA02",
+                           "N05AC02", "N05AA04", "N05AF04"
+  )
+  # prelim_checks$drugs1 is TRUE if the patient has tow or more of listed drugs.
+  prelim_checks$drugs1 <- check_matches(df,
+                                        column_string = drug_string,
+                                        codes = prelim_codes$drugs1,
+                                        match = "any",
+                                        exceptions = NULL,
+                                        more_than = 1)
+
+
+  # all_prelims is a logical vector with one entry per patient.
+  # TRUE if the patient is TRUE for each element of 'prelim_checks'.
+  all_prelims <- Reduce(x = prelim_checks, f = "&")
+
+
+  output <- ifelse(all_prelims, "STOPP-M1", "Appropriate")
 
   return(output)
 }
